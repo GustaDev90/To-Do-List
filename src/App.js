fe6dab7329c39a9c82b11d63e595aade0fa75c78
GustaDev <gustagames0912@gmail.com>
2024-12-17 18:34:43 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ export default function App() {
            setInput("");
 
         } else {
+            setTarefas([...tarefas, input])
             setError("");
             setInput("");
         }
